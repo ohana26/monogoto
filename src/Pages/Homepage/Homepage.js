005f2361ec6a8ba247ea51430ce4a8366df1f8c8
Chen Ochana <chen@almond-qms.com>
2022-03-25 15:48:51 +0300

    add logout button

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Table from "../../Components/Table/Table.js";
 
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
   const [data, SetData] = useState([]);
+
+  const navigate = useNavigate();
 
   const request = async () => {
     const { data } = await Axios.get("http://localhost:1880/symbolsList");
@@ -16,13 +20,20 @@ export default function LoginPage() {
     request();
   }, []);
 
+  const Logout = () => {
+    navigate("/");
+  };
+
   return (
     <div className="container bg-light bg-gradient">
       <nav
         className="navbar navbar-light"
-        style={{ backgroundColor: "#e3f2fd", padding: "15px" }}
+        style={{ "backgroundColor": "#e3f2fd", padding:"15px" }}
       >
         <h3>Mongoto</h3>
+        <button className="btn btn-secondary" id="logout" onClick={Logout}>
+          LogOut
+        </button>
       </nav>
       <hr />
       <div className="row">
