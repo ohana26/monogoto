894b2dfb2621837511321833534124199ded6a1b
Chen Ochana <chen@almond-qms.com>
2022-03-25 15:25:03 +0300

    create req to get all coins names also at red node

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,19 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function LoginPage() {
+  const [data, SetData] = useState([]);
 
+  const request = async () => {
+    const { data } = await Axios.get("http://localhost:1880/symbolsList");
+    console.log(data)
+    if (data.length > 0) {
+      SetData(data);
+    }
+  };
+  useEffect(() => {
+    request();
+  }, []);
 
-  return (
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 }
